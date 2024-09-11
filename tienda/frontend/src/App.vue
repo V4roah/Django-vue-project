4af7beea352ba99340a7d499a5fa6784fedaef9f
v4roah <dafvargasro@gmail.com>
2024-09-11 17:08:16 -0500

    learning some Vue

--- tienda/frontend/src/App.vue
+++ tienda/frontend/src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container my-4">
+    <h2 class="text-primary">eventos</h2>
+    <h6>Botones</h6>
+    <button
+      class="btn btn-primary"
+      v-on:click="hacerClick('mensaje enviado desde la etiqueta azul')"
+    >
+      Click Aqui
+    </button>
+    <button
+      class="btn btn-success"
+      @click="hacerClick('mensaje enviado desde la etiqueta verde')"
+    >
+      Click Aqui
+    </button>
+    <hr />
+    <h6>MODIFICADORES</h6>
+    <button
+      class="btn btn-warning"
+      @click="queBotonPresiono('Presiono el boton de la izquierda')"
+    >
+      IZQUIERDA
+    </button>
+    <button
+      class="btn btn-info"
+      @click.middle="queBotonPresiono('Boton central')"
+    >
+      CENTRO
+    </button>
+    <button
+      class="btn btn-success"
+      @click.right.prevent="queBotonPresiono('Boton derecho')"
+    >
+      DERECHO
+    </button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+const hacerClick = (mensaje) => {
+  console.log(mensaje);
+};
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const queBotonPresiono = (mensaje) => {
+  console.log(mensaje);
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  -webkit-font-smoothing: antialised;
+  -moz-osx-font-smoothing: graysclase;
   margin-top: 60px;
 }
 </style>
