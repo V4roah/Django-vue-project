4148e98e2ffdd62ef144f2387c4e2acfa7758a00
v4roah <dafvargasro@gmail.com>
2024-09-11 20:10:32 -0500

    learning some Vue 3.0

--- tienda/frontend/src/App.vue
+++ tienda/frontend/src/App.vue
@@ -47,11 +47,35 @@
     <button class="btn btn-sm btn-warning" @click="decrementar">
       DECREMENTAR
     </button>
+
+    <h5>RESULTADO = {{ resultado }}</h5>
+
+    <hr />
+
+    <h6>Variables computadas y objetos</h6>
+
+    <div class="card" style="width: 18rem">
+      <img src="#" class="card-img-top" alt="#" />
+      <div class="card-body">
+        <h5 class="card-title">{{ paisCorregido }}</h5>
+        <p class="card-text">{{ paises[contadorPaises].capital }}</p>
+        <p class="card-text">{{ paises[contadorPaises].moneda }}</p>
+        <p class="text-secondary text-center">
+          {{ contadorPaises + 1 }} de {{ totalPaises }}
+        </p>
+        <button class="btn btn-sm btn-success" @click="anterior">
+          Anterior
+        </button>
+        <button class="btn btn-sm btn-success" @click="siguiente">
+          Siguiente
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const hacerClick = (mensaje) => {
   console.log(mensaje);
@@ -69,9 +93,58 @@ const decrementar = () => {
   contador.value <= 0 ? (contador.value = 0) : contador.value--;
 };
 
-const prueba = 0;
+const valorUnitario = 33.33;
 
-let contador = ref(0);
+const contador = ref(0);
+
+const resultado = computed(() => {
+  let calculo = parseFloat((valorUnitario * contador.value).toFixed(2));
+  return calculo + 10;
+});
+
+const paises = [
+  {
+    bandera: "link",
+    pais: "Colombia",
+    capital: "Bogota",
+    moneda: "Peso Colombiano",
+  },
+  {
+    bandera: "link",
+    pais: "argentina",
+    capital: "Buenos Aires",
+    moneda: "Peso Argentino",
+  },
+  {
+    bandera: "link",
+    pais: "Mexico",
+    capital: "Ciudad de Mexico",
+    moneda: "Peso Mexicano",
+  },
+];
+
+const contadorPaises = ref(0);
+const totalPaises = paises.length;
+
+const siguiente = () => {
+  contadorPaises.value++;
+  if (contadorPaises.value >= totalPaises) {
+    contadorPaises.value = 0;
+  }
+};
+
+const anterior = () => {
+  if (contadorPaises.value <= 0) {
+    contadorPaises.value = totalPaises;
+  }
+  contadorPaises.value--;
+};
+
+const paisCorregido = computed(() => {
+  const paisOriginal = paises[contadorPaises.value].pais.toLowerCase();
+
+  return paisOriginal.substring(0, 1).toUpperCase() + paisOriginal.substring(1);
+});
 </script>
 
 <style>
